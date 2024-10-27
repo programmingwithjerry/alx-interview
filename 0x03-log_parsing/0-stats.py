d29622b17d599faf390bbf59144d28735753b65c
programmingwithjerry <eneyellowj@gmail.com>
2024-10-27 12:36:07 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,7 +7,7 @@ total_file_size = 0
 status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
-# Function to print the statistics
+# This Function prints the statistics
 def print_statistics():
     print(f"File size: {total_file_size}")
     for code in sorted(status_counts):
