54debab586e570caf6c8620e21459fb095736a16
Programmingwithjerry <eneyellowj@gmail.com>
2024-10-31 12:17:01 +0100

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Determines if a given data set represents a valid UTF-8 encoding.
-"""
-
 
 def validUTF8(data):
     """
+    Determines if a given data set represents a valid UTF-8 encoding
     Parameters:
     data (list of int): List of integers where each integer
     represents one byte of data.
